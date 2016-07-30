e89d6c8423d853c8f2500b0646aed6b0b7524a12
data-doge <eugene@enspiral.com>
2016-07-30 17:29:31 -0400

    can add rows via cmd line

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 var GoogleSpreadsheet = require('google-spreadsheet')
 var creds = require('./.credentials.json')
 var doc = new GoogleSpreadsheet(creds.spreadsheet_key)
@@ -5,11 +7,25 @@ var doc = new GoogleSpreadsheet(creds.spreadsheet_key)
 var type = process.argv[2]
 var text = process.argv[3]
 
+switch (type) {
+  case '+':
+    type = 'positive'; break
+  case '-':
+    type = 'negative'; break
+  default:
+    throw 'ur type is fucked brah'
+}
+
+if (text[text.length - 1] !== '.') {
+  text += '.'
+}
+
 doc.useServiceAccountAuth(creds, function () {
   doc.getInfo(function (err, info) {
     if (err) throw err
     var sheet = info.worksheets[0]
-    sheet.addRow({type: type, text: text}, function (err) {
+    var row = { type: type, text: text }
+    sheet.addRow(row, function (err) {
       if (err) throw err
 
       console.log('added yall')
