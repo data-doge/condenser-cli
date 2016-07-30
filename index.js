a9d0ed41dc7b21e3791e25940205483867b803ad
data-doge <eugene@enspiral.com>
2016-07-30 17:03:04 -0400

    can add rows to google sheet

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 var GoogleSpreadsheet = require('google-spreadsheet')
 var creds = require('./.credentials.json')
-
 var doc = new GoogleSpreadsheet(creds.spreadsheet_key)
+
+var type = process.argv[2]
+var text = process.argv[3]
 
 doc.useServiceAccountAuth(creds, function () {
   doc.getInfo(function (err, info) {
     if (err) throw err
     var sheet = info.worksheets[0]
-    sheet.getRows(function (err, rows) {
-      console.log('rows: ', rows)
+    sheet.addRow({type: type, text: text}, function (err) {
+      if (err) throw err
+
+      console.log('added yall')
     })
   })
 })
